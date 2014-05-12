60acf7532d804d1b7cd7f9a5d90680cbe0c6384f
timsvoice <timsethvoice@gmail.com>
2014-05-12 14:08:31 -0400

    added juniors method

--- student.rb
+++ student.rb
@@ -1,30 +1,46 @@
 class Student
+ 
+  attr_reader :first_name, :last_name, :grade
+  def initialize(first_name, last_name, grade)
+    @first_name = first_name
+    @last_name = last_name
+    @grade = grade
+  end
 
-	attr_reader :first_name, :last_name, :grade
-	def initialize(first_name, last_name, grade)
-		@first_name = first_name
-		@last_name = last_name
-		@grade = grade
-	end
+  def senior?
+    grade == 12
+  end
 
-	def senior?
-		grade == 12
-	end
+  def junior?
+    grade == 11
+  end
 
-	def to_s
-		"#{last_name}, #{first_name}"
-	end
+  def to_s
+    "#{last_name}, #{first_name}, #{grade}"    
+  end
 end
 
 def seniors(students)
-	students.select { |student| student.senior? }
+  students.select { |student| student.senior? }
 end
 
-fred = Student.new("Fred", "James", 12)
-sarah = Student.new("Sarah", "Smith", 12)
-jack = Student.new("Jack", "Gong", 11)
-all_students = [fred, sarah, jack]
+def juniors(students)
+  students.select { |student| student.junior? }
+end
+
+tim = Student.new("jack", "gong", 11)
+fred = Student.new("fred", "james", 12)
+jane = Student.new("sarah", "smith", 12)
+all_students = [tim, fred, jane]
+
+puts "Seniors"
 
 seniors(all_students).each do |student|
-	puts student
-end
+  puts student
+end 
+
+puts "Juniors"
+
+juniors(all_students).each do |student|
+  puts student
+end 
